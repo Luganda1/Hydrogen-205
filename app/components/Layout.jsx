9c6ed4ae4f3ebae1176174b03a77d859753280a6
Tonny Luganda <62723878+Luganda1@users.noreply.github.com>
2023-04-18 15:02:21 -0700

    Initial Commit

---
+++ app/components/Layout.jsx
@@ -0,0 +1,7 @@
+export function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen antialiased bg-neutral-50">
+      <h2>Talk to the king him self and we shall be good for sure</h2>
+    </div>
+  );
+}
